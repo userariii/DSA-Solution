71ccf2f9f7a9311701f54a8feb19bd34f0bbcd19
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-04-17 12:33:57 +0530

    DSA-Array: Sorting of 3rd largest & 2nd smallest
    The method will return multiplication of 3rd largest & 2nd smallest number

---
+++ Array/ThirdLargestNumber.java
@@ -0,0 +1,64 @@
+package Array;
+
+import java.util.Scanner;
+public class ThirdLargestNumber {
+
+    /*
+    Question:
+    There going to be 5-digit numbers,
+    and the result will be the product of 3rd largest digit
+    & 2nd smallest digit
+    */
+
+//    Main method starts here...
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 5 integer value: ");
+        int[] fiveIntegerNumber = new int[5];
+        for (int i = 0; i <fiveIntegerNumber.length; i++) {
+            fiveIntegerNumber[i] = sc.nextInt();
+        }
+        int multi = largestValue(fiveIntegerNumber);
+        System.out.println("Third Largest & Second Smallest element is: "+multi);
+    }
+
+    /*
+    --------------------------------------
+          Driver code starts here...
+    --------------------------------------
+    */
+
+    public static int largestValue(int[] arr) {
+//     Sorting here the largest element in an Array
+        int largestElement = Integer.MIN_VALUE;
+        for (int i=0; i < arr.length; i++ ) {
+            if (arr[i] > largestElement) {
+                largestElement = arr[i];
+            }
+        }
+//     Sorting second-largest element in an Array
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] > secondLargest && arr[i] != largestElement)
+                secondLargest = arr[i];
+        }
+//     Sorting here the third-largest element in an Array
+        int thirdLargest = Integer.MIN_VALUE;
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] > thirdLargest && arr[i] != secondLargest && arr[i] != largestElement)
+                thirdLargest = arr[i];
+        }
+//      Sorting here the second-smallest element in an Array
+        int small = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] < small) {
+                secondSmallest = small;
+                small = arr[i];
+            } else if (arr[i] < secondSmallest && arr[i] != small) {
+                secondSmallest = arr[i];
+            }
+        }
+        return (thirdLargest*secondSmallest);
+    }
+}
